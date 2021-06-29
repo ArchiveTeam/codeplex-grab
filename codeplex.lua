d3661a9fe4c17679d40d5ef4df5d06b4b6393e35
arkiver <arkiver@protonmail.com>
2021-06-29 02:57:40 +0200

    initial

---
+++ codeplex.lua
@@ -0,0 +1,315 @@
+dofile("table_show.lua")
+dofile("urlcode.lua")
+local urlparse = require("socket.url")
+local http = require("socket.http")
+JSON = (loadfile "JSON.lua")()
+
+local item_dir = os.getenv('item_dir')
+local warc_file_base = os.getenv('warc_file_base')
+local item_type = nil
+local item_name = nil
+local item_value = nil
+
+local url_count = 0
+local tries = 0
+local downloaded = {}
+local addedtolist = {}
+local abortgrab = false
+
+local outlinks = {}
+
+local bad_items = {}
+
+if not urlparse or not http then
+  io.stdout:write("socket not corrently installed.\n")
+  io.stdout:flush()
+  abortgrab = true
+end
+
+local ids = {}
+
+for ignore in io.open("ignore-list", "r"):lines() do
+  downloaded[ignore] = true
+end
+
+abort_item = function(item)
+  abortgrab = true
+  if not item then
+    item = item_name
+  end
+  if not bad_items[item] then
+    io.stdout:write("Aborting item " .. item .. ".\n")
+    io.stdout:flush()
+    bad_items[item] = true
+  end
+end
+
+read_file = function(file)
+  if file then
+    local f = assert(io.open(file))
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+allowed = function(url, parenturl)
+  if string.match(urlparse.unescape(url), "[<>\\%*%$%^%[%],%(%){}]") then
+    return false
+  end
+
+  local tested = {}
+  for s in string.gmatch(url, "([^/]+)") do
+    if not tested[s] then
+      tested[s] = 0
+    end
+    if tested[s] == 6 then
+      return false
+    end
+    tested[s] = tested[s] + 1
+  end
+
+  for s in string.gmatch(url, "([^%.=/]+)") do
+    if ids[s] then
+      return true
+    end
+  end
+
+  if not string.match(url, "^https?://[^/]*codeplex%.com") then
+    outlinks[url] = true
+  end
+
+  return false
+end
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  return false
+end
+
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  local urls = {}
+  local html = nil
+  
+  downloaded[url] = true
+
+  local function check(urla)
+    local origurl = url
+    local url = string.match(urla, "^([^#]+)")
+    local url_ = string.match(url, "^(.-)%.?$")
+    url_ = string.gsub(url_, "&amp;", "&")
+    url_ = string.match(url_, "^(.-)%s*$")
+    url_ = string.match(url_, "^(.-)%??$")
+    url_ = string.match(url_, "^(.-)&?$")
+    url_ = string.match(url_, "^(.-)/?$")
+    if (downloaded[url_] ~= true and addedtolist[url_] ~= true)
+      and allowed(url_, origurl) then
+      table.insert(urls, { url=url_ })
+      addedtolist[url_] = true
+      addedtolist[url] = true
+    end
+  end
+
+  local function checknewurl(newurl)
+    if string.match(newurl, "\\[uU]002[fF]") then
+      return checknewurl(string.gsub(newurl, "\\[uU]002[fF]", "/"))
+    end
+    if string.match(newurl, "^https?:////") then
+      check(string.gsub(newurl, ":////", "://"))
+    elseif string.match(newurl, "^https?://") then
+      check(newurl)
+    elseif string.match(newurl, "^https?:\\/\\?/") then
+      check(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^\\/") then
+      checknewurl(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^//") then
+      check(urlparse.absolute(url, newurl))
+    elseif string.match(newurl, "^/") then
+      check(urlparse.absolute(url, newurl))
+    elseif string.match(newurl, "^%.%./") then
+      if string.match(url, "^https?://[^/]+/[^/]+/") then
+        check(urlparse.absolute(url, newurl))
+      else
+        checknewurl(string.match(newurl, "^%.%.(/.+)$"))
+      end
+    elseif string.match(newurl, "^%./") then
+      check(urlparse.absolute(url, newurl))
+    end
+  end
+
+  local function checknewshorturl(newurl)
+    if string.match(newurl, "^%?") then
+      check(urlparse.absolute(url, newurl))
+    elseif not (string.match(newurl, "^https?:\\?/\\?//?/?")
+      or string.match(newurl, "^[/\\]")
+      or string.match(newurl, "^%./")
+      or string.match(newurl, "^[jJ]ava[sS]cript:")
+      or string.match(newurl, "^[mM]ail[tT]o:")
+      or string.match(newurl, "^vine:")
+      or string.match(newurl, "^android%-app:")
+      or string.match(newurl, "^ios%-app:")
+      or string.match(newurl, "^%${")) then
+      check(urlparse.absolute(url, newurl))
+    end
+  end
+
+  if allowed(url, nil) and status_code == 200 and not string.match(url, "%.zip$") then
+    html = read_file(file)
+    if string.match(url, "%?p=") then
+      check("https://codeplexarchive.blob.core.windows.net/archive/projects/" .. item_value .. "/" .. item_value .. ".zip")
+      check("https://" .. item_value .. ".codeplex.com/")
+      check("https://archive.codeplex.com/metadata/" .. item_value .. ".json")
+      check("https://archive.codeplex.com/projects/" .. item_value .. "/discussions/discussions.json")
+      check("https://archive.codeplex.com/projects/" .. item_value .. "/issues/issues.json")
+    end
+    if string.match(url, "/projects/") and string.match(url, "%.json$") then
+      local sort = string.match(url, "([^%./]+)%.json$")
+      for _, d in pairs(JSON:decode(html)) do
+        local id = d["Id"]
+        if sort == "issues" then
+          check("https://archive.codeplex.com/projects/" .. item_value .. "/issues/" .. id .. "/" .. id .. ".json")
+        elseif sort == "discussions" then
+          check("https://archive.codeplex.com/projects/" .. item_value .. "/discussions/" .. id .. ".json")
+        end
+      end
+    end
+    html = string.gsub(html, '\\"', '"')
+    for newurl in string.gmatch(string.gsub(html, "&quot;", '"'), '([^"]+)') do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(string.gsub(html, "&#039;", "'"), "([^']+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, ">%s*([^<%s]+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "[^%-]href='([^']+)'") do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, '[^%-]href="([^"]+)"') do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, "%(([^%)]+)%)") do
+      checknewurl(newurl)
+    end
+  end
+
+  return urls
+end
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  status_code = http_stat["statcode"]
+
+  local match = string.match(url["url"], "^https://archive.codeplex.com/%?p=(.+)$")
+  local type_ = "p"
+  if match then
+    abortgrab = false
+    ids[match] = true
+    item_value = match
+    item_type = type_
+    item_name = type_ .. ":" .. match
+    io.stdout:write("Archiving item " .. item_name .. ".\n")
+    io.stdout:flush()
+  end
+  
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. "  \n")
+  io.stdout:flush()
+
+  if status_code == 404 and
+    (
+      string.match(url["url"], "vthumb.+%.jpg$")
+      or string.match(url["url"], "%.zip$")
+    ) then
+    return wget.actions.EXIT
+  end
+
+  if status_code >= 300 and status_code <= 399 then
+    local newloc = urlparse.absolute(url["url"], http_stat["newloc"])
+    if downloaded[newloc] or addedtolist[newloc]
+      or not allowed(newloc, url["url"]) then
+      tries = 0
+      return wget.actions.EXIT
+    end
+  end
+  
+  if status_code >= 200 and status_code <= 399 then
+    downloaded[url["url"]] = true
+  end
+
+  if abortgrab then
+    abort_item()
+    return wget.actions.ABORT
+  end
+
+  if status_code == 0 or status_code >= 400 then
+    io.stdout:write("Server returned " .. http_stat.statcode .. " (" .. err .. "). Sleeping.\n")
+    io.stdout:flush()
+    local maxtries = 5
+    if not allowed(url["url"], nil) then
+      maxtries = 3
+    end
+    if tries >= maxtries then
+      io.stdout:write("I give up...\n")
+      io.stdout:flush()
+      tries = 0
+      abort_item()
+      return wget.actions.ABORT
+    else
+      os.execute("sleep " .. math.floor(math.pow(2, tries)))
+      tries = tries + 1
+      return wget.actions.CONTINUE
+    end
+  end
+
+  tries = 0
+
+  local sleep_time = 0
+
+  if sleep_time > 0.001 then
+    os.execute("sleep " .. sleep_time)
+  end
+
+  return wget.actions.NOTHING
+end
+
+wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total_downloaded_bytes, total_download_time)
+  local items = nil
+  for item, _ in pairs(outlinks) do
+    print('found item', item)
+    if items == nil then
+      items = item
+    else
+      items = items .. "\0" .. item
+    end
+  end
+  if items ~= nil then
+    local tries = 0
+    while tries < 10 do
+      local body, code, headers, status = http.request(
+        "http://blackbird-amqp.meo.ws:23038/urls-jm6jwwu7owp38du/",
+        items
+      )
+      if code == 200 or code == 409 then
+        break
+      end
+      io.stdout:write("Could not queue items.\n")
+      io.stdout:flush()
+      os.execute("sleep " .. math.floor(math.pow(2, tries)))
+      tries = tries + 1
+    end
+    if tries == 10 then
+      abort_item()
+    end
+  end
+end
+
+wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  if abortgrab then
+    abort_item()
+    return wget.exits.IO_FAIL
+  end
+  return exit_status
+end
+
